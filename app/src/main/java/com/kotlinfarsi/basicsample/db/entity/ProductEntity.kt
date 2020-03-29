0a59176de5f93c8f29c667b4811b9cdd6d811dab
Sina Darvishi <sinadarvi@gmail.com>
2020-03-29 16:51:21 +0430

    product entity added

--- app/src/main/java/com/kotlinfarsi/basicsample/db/entity/ProductEntity.kt
+++ app/src/main/java/com/kotlinfarsi/basicsample/db/entity/ProductEntity.kt
@@ -1,4 +1,15 @@
 package com.kotlinfarsi.basicsample.db.entity
 
-//TODO: Step 1 - Creating Product table
-class ProductEntity
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.kotlinfarsi.basicsample.model.Product
+
+
+@Entity(tableName = "products")
+data class ProductEntity @JvmOverloads constructor(
+    @PrimaryKey
+    override var id: Int? = null,
+    override var name: String? = null,
+    override var description: String? = null,
+    override var price: Int? = null
+) : Product
