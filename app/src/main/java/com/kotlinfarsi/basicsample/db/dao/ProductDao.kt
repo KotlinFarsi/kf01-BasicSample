064f541923102b01baea27e57cd24f3acb04fd49
Sina Darvishi <sinadarvi@gmail.com>
2020-03-29 20:50:33 +0430

    creating product dao

--- app/src/main/java/com/kotlinfarsi/basicsample/db/dao/ProductDao.kt
+++ app/src/main/java/com/kotlinfarsi/basicsample/db/dao/ProductDao.kt
@@ -1,4 +1,20 @@
 package com.kotlinfarsi.basicsample.db.dao
 
-//TODO: Step 2 - Creating a Dao
-interface ProductDao
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.kotlinfarsi.basicsample.db.entity.ProductEntity
+
+@Dao
+interface ProductDao {
+    @Query("SELECT * FROM products")
+    fun loadAllProducts(): LiveData<List<ProductEntity>>
+
+    @Query("SELECT * FROM products where id = :productId")
+    fun loadProduct(productId: Int): LiveData<ProductEntity>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertAll(products: List<ProductEntity>)
+}
