d8c35a3deb8987b095a8a578959a1eee8e1331fc
Sina Darvishi <sinadarvi@gmail.com>
2020-03-30 00:39:48 +0430

    adding todos for step 6

--- app/src/main/java/com/kotlinfarsi/basicsample/ui/product/ProductViewModel.kt
+++ app/src/main/java/com/kotlinfarsi/basicsample/ui/product/ProductViewModel.kt
@@ -10,10 +10,13 @@ class ProductViewModel(
     private val productId: Int
 ) :
     AndroidViewModel(application) {
+    //TODO: Part 6 - (1) create a observable field for product view to include
 
     fun getComments(){
+        //TODO: Part 6 - (2) create a method for loading comments
     }
 
     fun getObservableProduct() {
+        //TODO: Part 6 - (3) create a method for load a product
     }
 }
